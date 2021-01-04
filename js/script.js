dc7efd4e7e85eb613098d89b513e2834448a5bbb
hager mohamed tawfik <hagertawfik2000@gmail.com>
2021-01-04 14:36:32 +0200

    update the task

--- js/script.js
+++ js/script.js
@@ -1,52 +1,27 @@
 
-//button 1 with his action itis show this is home content
-var button1 = document.getElementById("btn1");
-hom =  button1.innerText ;
-function action(){
-    var div = document.getElementById("pm");
-    div.innerHTML ="This is "+hom+" "+ " content";
-    
-}
-
-button1.onclick = action;
-
-
-//button2 with his action itis show this is profile content
-var button2 = document.getElementById("btn2");
-prof =  button2.innerText ;
-function action1(){
-    var div = document.getElementById("pm");
-    div.innerHTML="This is  " +prof+"  "+"  content";
- 
-}
-
-button2.onclick = action1;
-
-
-//button3 with his action itis show this is messages content
-var button3 = document.getElementById("btn3");
-var mess = button3.innerText ;
-function action3(){
-    var div = document.getElementById("pm");
- div.innerHTML= "This is  " + mess +" "+ "  content";
-
-}
-
-button3.onclick = action3;
-
-
-//button 4 with his action itis show this is settings content
-var button4 = document.getElementById("btn4");
-var sett = button4.innerText ;
-function action4(){
-    var div = document.getElementById("pm");
-   div.innerHTML= "This is  " + sett + " " + "  content";
+//function for the tabs
+function openbtn(evt, Namebtn) {
+    // Declare all variables
+    var i, btncontent, tapslink;
   
-}
-
-button4.onclick = action4;
-
-
-
-
+    // Get all elements with class="btncontent" and hide them
+    btncontent = document.getElementsByClassName("btncontent");
+    for (i = 0; i < btncontent.length; i++) {
+        btncontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tapslink" and remove the class "active"
+    tapslink = document.getElementsByClassName("tapslink");
+    for (i = 0; i < tapslink.length; i++) {
+      tapslink[i].className = tapslink[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(Namebtn).style.display = "block";
+    evt.currentTarget.className += " active"; 
+    
+  }
+ 
+  
+  
 
